33a3a34216a781ff53cb9ce6efd372fc74c409a0
柳珋 <2023243524@qq.com>
2026-07-29 17:07:29 +0800

    Move files into Introduction to Programming folder

--- solutions/Introduction to Programming/1-two-sum.c
+++ solutions/Introduction to Programming/1-two-sum.c
@@ -1,1 +1,26 @@
+// 题目: 两数之和
+// 题号: 1
+// 语言: c
+// 提交时间: 2026-07-25 09:36:15
 
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                int* result = (int*)malloc(2 * sizeof(int));//创建返回的新整数数组result
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
+    return 0;
+}
