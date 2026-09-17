73c5851f97805f5729c52e12a2435029e420089b
Liuliu0917 <Liuliu0917@users.noreply.github.com>
2026-09-17 13:05:27 +0000

    Sync LeetCode submissions

---
+++ solutions/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,39 @@
+// 题目: 在排序数组中查找元素的第一个和最后一个位置
+// 题号: 34
+// 语言: cpp
+// 提交时间: 2026-09-16 15:18:24
+
+class Solution {
+    // lower_bound 返回最小的满足 nums[i] >= target 的下标 i
+    // 如果数组为空，或者所有数都 < target，则返回 nums.size()
+    // 要求 nums 是非递减的，即 nums[i] <= nums[i + 1]
+    int lower_bound(vector<int>& nums, int target) {
+        int left = 0, right = (int) nums.size() - 1; // 闭区间 [left, right]
+        while (left <= right) { // 区间不为空
+            // 循环不变量：
+            // nums[left-1] < target
+            // nums[right+1] >= target
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1; // 范围缩小到 [left, mid-1]
+            } else {
+                left = mid + 1; // 范围缩小到 [mid+1, right]
+            }
+        }
+        // 循环结束后 left = right+1
+        // 此时 nums[left-1] < target 而 nums[left] = nums[right+1] >= target
+        // 所以 left 就是第一个 >= target 的元素下标
+        return left;
+    }
+
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start = lower_bound(nums, target);
+        if (start == nums.size() || nums[start] != target) {
+            return {-1, -1}; // nums 中没有 target
+        }
+        // 如果 start 存在，那么 end 必定存在
+        int end = lower_bound(nums, target + 1) - 1;
+        return {start, end};
+    }
+};
